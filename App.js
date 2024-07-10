115d35ad7cef0e98a612bf7b510f257b0be91fc3
Minjae Oh <ominj98@gmail.com>
2024-07-11 08:13:15 +0900

    refactor: 불필요한 코드 제거

--- App.js
+++ App.js
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {useColorScheme} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
